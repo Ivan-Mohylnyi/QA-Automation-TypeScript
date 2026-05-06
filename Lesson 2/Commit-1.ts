53f1460f28941de35dca3a6039a442a6ad18d67e
Ivan-Mohylnyi <vanya1998mogilnui@gmail.com>
2026-05-06 17:39:06 +0300

    Add commit

--- Lesson 2/Commit-1.ts
+++ Lesson 2/Commit-1.ts
@@ -1,3 +1,8 @@
 console.log('Hello, TypeScript!');
 let message: string = 'Welcome to TypeScript 6.0.3';
 console.log(message);
+
+function greet(name: string): string {
+    return `Hello, ${name}!`;
+}
+console.log(greet('TypeScript'));
