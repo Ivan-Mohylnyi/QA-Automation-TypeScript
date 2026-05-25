7d6e9d6f04630c1c8904e0748161a248a5c5b583
IvanMohylnyi <vanya1998mogilnui@gmail.com>
2026-05-25 13:20:00 +0300

    Deleted comments

--- Lesson-5/src/arrow-functions.js
+++ Lesson-5/src/arrow-functions.js
@@ -1,11 +1,8 @@
-// Arrow function for arithmetic multiplication of array elements
 const multiplyArrayElements = (array) => {
     return array.reduce((product, current) => {
-        // Convert the element to a number if possible
         const number = parseFloat(current);
         return !isNaN(number) ? product * number : product;
     }, 1);
 };
 
-// Exporting the function for possible use in other files
 export { multiplyArrayElements };
